b11c7b36e6595e9a5be630032b7fdcf7154b63d6
Quentin Franklin <qfranklin@gmail.com>
2024-10-27 08:57:00 -0400

    added vue project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'qfranklin/vue',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '52fa1157cfb583f984b42a36b20784fafe8bbc98',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'qfranklin/vue' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '52fa1157cfb583f984b42a36b20784fafe8bbc98',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

a12838712799431c32cfae297f09ab2d532e182c
Quentin Franklin <qfranklin906@gmail.com>
2024-11-16 13:12:09 -0500

    added editable birthday field

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -6,7 +6,21 @@
         <img src="/profile.jpg" alt="Profile Picture" class="profile-image">
         <div class="description">
           <p v-if="isLoggedIn">
-            <strong>Birthday:</strong> {{ formattedBirthday }}<br>
+            <strong>Birthday:</strong>
+            <div class="editable-field">
+              <input
+                v-if="isEditing"
+                type="date"
+                v-model="editableBirthday"
+                @blur="cancelEdit"
+              />
+              <span v-else @click="startEdit">{{ formattedBirthday }}</span>
+              <span v-if="!isEditing" class="edit-icon" @click="startEdit">✏️</span>
+            </div>
+            <a v-if="isEditing" @click="submitEdit" class="submit-link">Submit</a>
+            <p v-if="successMessage" class="success-message">{{ successMessage }}</p>
+            <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+            <br>
             <strong>Numerology:</strong> {{ numerology }}<br>
             <strong>Astrology:</strong> {{ astrology }}
           </p>
@@ -20,7 +34,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import axios from 'axios'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import { useUserStore } from '@/stores/user'
 
@@ -33,6 +48,10 @@ export default defineComponent({
     const userStore = useUserStore()
     const isLoggedIn = computed(() => userStore.isLoggedIn)
     const birthday = computed(() => userStore.birthday)
+    const editableBirthday = ref(userStore.birthday)
+    const isEditing = ref(false)
+    const successMessage = ref('')
+    const errorMessage = ref('')
 
     const getOrdinalSuffix = (day: number) => {
       if (day > 3 && day < 21) return 'th'
@@ -53,14 +72,46 @@ export default defineComponent({
       return `${month} ${day}${getOrdinalSuffix(day)}, ${year}`
     })
 
+    const startEdit = () => {
+      isEditing.value = true
+      editableBirthday.value = userStore.birthday
+    }
+
+    const cancelEdit = () => {
+      isEditing.value = false
+      editableBirthday.value = userStore.birthday
+    }
+
+    const submitEdit = async () => {
+      try {
+        const response = await axios.post('/api/update-birthday', {
+          birthday: editableBirthday.value
+        })
+        userStore.birthday = editableBirthday.value
+        successMessage.value = 'Birthday updated successfully!'
+        errorMessage.value = ''
+        isEditing.value = false
+      } catch (error) {
+        errorMessage.value = 'Failed to update birthday. Please try again.'
+        successMessage.value = ''
+      }
+    }
+
     const numerology = computed(() => userStore.numerology)
     const astrology = computed(() => userStore.astrology)
     return {
       isLoggedIn,
       birthday,
+      editableBirthday,
+      isEditing,
       formattedBirthday,
       numerology,
-      astrology
+      astrology,
+      startEdit,
+      cancelEdit,
+      submitEdit,
+      successMessage,
+      errorMessage
     }
   }
 })
@@ -88,6 +139,34 @@ export default defineComponent({
 
 .description {
   max-width: 600px;
+}
+
+.editable-field {
+  display: inline-flex;
+  align-items: center;
+  background-color: #f0f0f0;
+  padding: 0.5rem;
+  border-radius: 4px;
+}
+
+.edit-icon {
+  margin-left: 0.5rem;
+  cursor: pointer;
+  color: #888; /* Greyed out color */
+}
+
+.submit-link {
+  color: blue;
+  cursor: pointer;
+  margin-left: 0.5rem;
+}
+
+.success-message {
+  color: green;
+}
+
+.error-message {
+  color: red;
 }
 
 @media (min-width: 1024px) {

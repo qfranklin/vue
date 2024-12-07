0299fb2619c139e988b03070448816fab79019c1
Quentin Franklin <qfranklin906@gmail.com>
2024-12-07 12:33:50 -0500

    changed max price to a candlestick chart

--- src/components/SMAChartComponent.vue
+++ src/components/SMAChartComponent.vue
@@ -11,7 +11,7 @@
         {{ crypto }}
       </button>
     </div>
-    <canvas id="smaChart"></canvas>
+    <canvas id="cryptoChart"></canvas>
   </div>
 </template>
 
@@ -19,7 +19,9 @@
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
 import { Chart, registerables, Tooltip} from 'chart.js'
+import type { ChartData } from 'chart.js'
 import type { TooltipItem, TooltipModel, ActiveElement } from 'chart.js'
+import {CandlestickController, CandlestickElement, OhlcElement} from 'chartjs-chart-financial';
 import { subDays, format } from 'date-fns'
 import 'chartjs-adapter-date-fns'
 import { debounce } from 'lodash';
@@ -34,7 +36,7 @@ export interface TooltipPositionerMap {
   custom: (items: TooltipItem<'line'>[], eventPosition: { x: number; y: number }) => { x: number; y: number; xAlign: string; yAlign: string } | false;
 }
 
-Chart.register(...registerables)
+Chart.register(...registerables, CandlestickController, CandlestickElement, OhlcElement)
 
 Tooltip.positioners.custom = function(items: TooltipItem<'line'>[], eventPosition: { x: number; y: number }) {
   const pos = Tooltip.positioners.average.call(this as unknown as TooltipModel<'line'>, items as unknown as ActiveElement[], eventPosition);
@@ -61,12 +63,13 @@ Tooltip.positioners.custom = function(items: TooltipItem<'line'>[], eventPositio
 };
 
 export default {
-  name: 'SMAChartComponent',
+  name: 'CryptoChartComponent',
   setup() {
     const cryptos = ['Bitcoin', 'Ethereum', 'Solana', 'Monero'];
     const activeCrypto = ref('Bitcoin');
     const responseData = ref<Array<{
       date: string;
+      current_price: number;
       high_24h: number;
       low_24h: number;
       ma_10: number;
@@ -90,8 +93,9 @@ export default {
           }
         });
 
-        responseData.value = response.data.map((item: { date: string; high_24h: string; low_24h: string; ma_10: string; ma_50: string }) => ({
+        responseData.value = response.data.map((item: { date: string; current_price: string; high_24h: string; low_24h: string; ma_10: string; ma_50: string }) => ({
           date: format(new Date(item.date + 'T00:00:00'), 'yyyy-MM-dd'),
+          current_price: parseFloat(item.current_price),
           high_24h: parseFloat(item.high_24h),
           low_24h: parseFloat(item.low_24h),
           ma_10: parseFloat(item.ma_10),
@@ -108,48 +112,72 @@ export default {
     const fetchCryptoDataDebounced = debounce(fetchCryptoData, 300);
 
     const renderChart = () => {
-      const canvas = document.getElementById('smaChart') as HTMLCanvasElement
+      const canvas = document.getElementById('cryptoChart') as HTMLCanvasElement
       const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
       if (chartInstance.value) {
         chartInstance.value.destroy()
         chartInstance.value = null
       }
+
+      const low24hValues = responseData.value.map(item => item.low_24h).filter(value => !isNaN(value));
+      const high24hValues = responseData.value.map(item => item.high_24h).filter(value => !isNaN(value));
+
+      const candlestickData = responseData.value.map((item, index) => ({
+        x: new Date(item.date).getTime(),
+        o: index === 0 ? item.current_price : responseData.value[index - 1].current_price, // Open price
+        h: item.high_24h, // High price
+        l: item.low_24h, // Low price
+        c: item.current_price, // Close price
+      }));
+
+      const ma10Data = responseData.value.map(item => ({
+        x: new Date(item.date).getTime(),
+        y: item.ma_10,
+      }));
+
+      const ma50Data = responseData.value.map(item => ({
+        x: new Date(item.date).getTime(),
+        y: item.ma_50,
+      }));
+
+      const chartData: ChartData = {
+        labels: responseData.value.map(item => item.date),
+        datasets: [
+          {
+            label: 'Price',
+            type: 'candlestick',
+            data: candlestickData,
+            borderColor: '#333333',
+            backgroundColor: '#333333',
+          },
+          {
+            label: 'MA 10',
+            type: 'line',
+            data: ma10Data,
+            borderColor: 'rgba(51, 51, 51, 0.5)', // Lighter color
+            backgroundColor: 'rgba(51, 51, 51, 0.5)',
+            fill: false,
+            pointRadius: 0,
+            borderWidth: 1, // Thinner line
+            tension: 0.4 // Smooth line
+          },
+          {
+            label: 'MA 50',
+            type: 'line',
+            data: ma50Data,
+            borderColor: 'rgba(51, 51, 51, 0.3)', // Lighter color
+            backgroundColor: 'rgba(51, 51, 51, 0.3)',
+            fill: false,
+            pointRadius: 0,
+            borderWidth: 1, // Thinner line
+            tension: 0.4 // Smooth line
+          }
+        ]
+      }
+
       chartInstance.value = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: responseData.value.map(item => item.date),
-          datasets: [
-            {
-              label: 'Max Price',
-              data: responseData.value.map(item => item.high_24h),
-              borderColor: '#333333',
-              backgroundColor: '#333333',
-              fill: false,
-              pointRadius: 0,
-              tension: 0.4 // Smooth line
-            },
-            {
-              label: 'MA 10',
-              data: responseData.value.map(item => item.ma_10),
-              borderColor: 'rgba(51, 51, 51, 0.5)', // Lighter color
-              backgroundColor: 'rgba(51, 51, 51, 0.5)',
-              fill: false,
-              pointRadius: 0,
-              borderWidth: 1, // Thinner line
-              tension: 0.4 // Smooth line
-            },
-            {
-              label: 'MA 50',
-              data: responseData.value.map(item => item.ma_50),
-              borderColor: 'rgba(51, 51, 51, 0.3)', // Lighter color
-              backgroundColor: 'rgba(51, 51, 51, 0.3)',
-              fill: false,
-              pointRadius: 0,
-              borderWidth: 1, // Thinner line
-              tension: 0.4 // Smooth line
-            }
-          ]
-        },
+        type: 'candlestick',
+        data: chartData,
         options: {
           responsive: true,
           scales: {
@@ -158,9 +186,8 @@ export default {
             },
             y: {
               beginAtZero: false,
-              ticks: {
-                stepSize: 10000
-              }
+              min: Math.min(...low24hValues) - 1000, // Adjust min value
+              max: Math.max(...high24hValues) + 1000, // Adjust max value
             }
           },
           interaction: {
@@ -184,10 +211,18 @@ export default {
                   const index = context[0].dataIndex; // Use the index from the first dataset
                   const data = responseData.value[index];
                   const date = format(new Date(data.date + 'T00:00:00'), 'EEE, MMM d');
-                  const price = new Intl.NumberFormat('en-US', {
+                  const highPrice = new Intl.NumberFormat('en-US', {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
                   }).format(data.high_24h);
+                  const lowPrice = new Intl.NumberFormat('en-US', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  }).format(data.low_24h);
+                  const currentPrice = new Intl.NumberFormat('en-US', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  }).format(data.current_price);
                   const ma10 = new Intl.NumberFormat('en-US', {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
@@ -197,7 +232,7 @@ export default {
                     maximumFractionDigits: 2,
                   }).format(data.ma_50);
 
-                  return `${date}\nPrice: ${price}\nMA 10: ${ma10}\nMA 50: ${ma50}`;
+                  return `${date}\nHigh: ${highPrice}\nLow: ${lowPrice}\n5am EST: ${currentPrice}\nMA 10: ${ma10}\nMA 50: ${ma50}`;
                 },
                 label: function () {
                   return ''; // Remove dataset-specific labels

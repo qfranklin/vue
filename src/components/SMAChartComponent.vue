9831cc4f3ee863a44744d6934547c4c545996310
Quentin Franklin <qfranklin906@gmail.com>
2024-12-11 15:54:09 -0500

    changed default time series to hourly

--- src/components/SMAChartComponent.vue
+++ src/components/SMAChartComponent.vue
@@ -87,7 +87,7 @@ export default {
     }
     const times = Object.values(timeMapping)
     const activeCrypto = ref('bitcoin')
-    const activeTime = ref('7d')
+    const activeTime = ref('hourly')
     const responseData = ref<Array<{ date: string; current_price: number; high_24h: number; low_24h: number; ma_10: number; ma_50: number; rsi: number }>>([])
     const chartInstance = ref<Chart | null>(null)
     const loading = ref(false)

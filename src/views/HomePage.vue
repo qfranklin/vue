322a62d9bb2176eb7c5e77ec63e7043c95d6311e
Quentin Franklin <qfranklin906@gmail.com>
2024-12-25 18:49:19 -0500

    removed notes from home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,22 +1,17 @@
 <template>
   <section>
-    <div v-if="isLoggedIn">
-      <NotesComponent />
-    </div>
     <FinancialChartComponent />
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue'
-import NotesComponent from '@/components/NotesComponent.vue'
 import FinancialChartComponent from '@/components/FinancialChartComponent.vue'
 import { useUserStore } from '@/stores/user'
 
 export default defineComponent({
   name: 'HomePage',
   components: {
-    NotesComponent,
     FinancialChartComponent
   },
   setup() {
